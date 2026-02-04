6298c340eafc1faea477bd37ca701555965a7131
Nicolas Azcuy <nico.azcuy@gmail.com>
2026-02-04 17:53:36 -0300

    Configuración de app.js para websockets

--- src/app.js
+++ src/app.js
@@ -1,4 +1,7 @@
 import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+import handlebars from "express-handlebars";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
 import morgan from "morgan";
@@ -6,12 +9,48 @@ import morgan from "morgan";
 const app = express();
 const PORT = 8080;
 
+app.engine("handlebars", handlebars.engine());
+app.set("views", "./src/views");
+app.set("view engine", "handlebars");
+
+const httpServer = createServer(app);
+const io = new Server(httpServer);
+
+app.use((req, res, next) => {
+  req.io = io;
+  next();
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan("dev"));
+
+app.use(express.static("public"));
+
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
 
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en http://localhost:${PORT}`);
+app.get(/realTimeProducts/, (req, res) => {
+  res.render("realTimeProducts", { 
+    title: "Products en tiempo real"
+  });
+});
+
+app.get("/", async (req, res) => {
+  res.render("home", {
+    title: "Tienda de productos"
+  });
+});
+
+io.on("connection", (socket) => {
+  console.log(`Nuevo cliente conectado: ${socket.id}`);
+
+  socket.on("disconnect", () => {
+    console.log(`Cliente desconectado: ${socket.id}`);
+  });
+});
+
+httpServer.listen(PORT, () => {
+  console.log(`Servidor HTTP en http://localhost:${PORT}`);
+  console.log(`WebSocket activo en http://localhost:${PORT}`);
 });
